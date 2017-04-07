ffedc7fe6e9bfca783d55083f97a8f6c173c54ac
lleonardoll <leofrederiche@gmail.com>
2017-04-07 16:09:33 -0300

    #1 - Controller <new> <list>

--- spec/controllers/lists_controller_spec.rb
+++ spec/controllers/lists_controller_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 describe ListsController do
-    it 'Verify array <lists> in Index' do
+    it '#INDEX - Verify array <lists> in Index' do
       list = FactoryGirl.create(:list)
       get :index
       expect(assigns(:lists)).to eq([list])
+    end
+
+    it '#NEW Verify variable <list> to create new list' do
+      get :new
+      expect(assigns(:list)).to be_a_new(List)
     end
 end
