9a3e4947ad1d9f1afa5fc82de815fdbf0aac71d4
lleonardoll <leofrederiche@gmail.com>
2017-04-07 15:31:37 -0300

    #1.1 - Registrar lista - #new/create | RSpec model

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe List, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe List do
+  it 'Create a valid list' do
+    expect {
+      list = FactoryGirl.build(:list)
+      list.save
+    }.to change(List, :count).by(1)
+  end
+
+  it 'Create a invalid list' do
+    expect {
+      list = FactoryGirl.build(:invalid_list)
+      list.save
+    }.to_not change(List, :count)
+  end
 end
