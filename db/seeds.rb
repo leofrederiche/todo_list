dd4840078377de0229df495c8870de778a5d3b71
lleonardoll <leofrederiche@gmail.com>
2017-04-07 15:59:51 -0300

    #3 - Exibição de listas no index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+list = List.new
+list.title = 'Tarefas para casa'
+list.description = 'Aqui jás todas as terefas que devem ser cumpridas para limpeza da casa.'
+list.save
+
+list = List.new
+list.title = 'Afazeres'
+list.description = 'Afazeres do dia-a-dia (estudar, igreja, jogar, tocar violão/violino).'
+list.save
