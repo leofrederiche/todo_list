f88c3346ba9bf6feef347719d90f7a7e4c8c7db4
lleonardoll <leofrederiche@gmail.com>
2017-04-10 18:45:11 -0300

    #1 - Formulario com modal e AngularJS

--- app/assets/javascripts/api/angular/lists/lists_api.js
+++ app/assets/javascripts/api/angular/lists/lists_api.js
@@ -11,7 +11,7 @@
     return $resource('', {id: '@id'}, {
       index: {url: '/api/v1/lists', method: 'GET', isArray: true},
       // new: {url: '/api/users/new', method: 'GET'},
-      // create: {url: '/api/users', method: 'POST'},
+      create: {url: '/api/v1/lists', method: 'POST'},
       // edit: {url: '/api/users/:id/edit', method: 'GET'},
       // update: {url: '/api/users/:id', method: 'PATCH'},
       // destroy: {url: '/api/users/:id', method: 'DELETE'}
--- app/assets/javascripts/api/angular/lists/lists_index.js
+++ app/assets/javascripts/api/angular/lists/lists_index.js
@@ -5,19 +5,30 @@
     .module('app')
     .controller('ListIndex', ListIndex);
 
-  ListIndex.$inject = ['listAPI'];
+  ListIndex.$inject = ['listAPI', '$scope'];
 
-  function ListIndex(listAPI) {
+  function ListIndex(listAPI, $scope) {
 
     var vm = this;
     // Methods for view
-    vm.load = load();
+    vm.load = load;
+    vm.create = create;
 
     function load(){
       listAPI.index(function(data) {
         vm.lists = data;
       }, function (errors) {
         console.log('errors', errors);
+      });
+    }
+
+    function create(response){
+      listAPI.create({list: response},function(data){
+        load();
+        $('#newListModal').modal('hide');
+        $scope.list = null;
+      }, function(errors) {
+          console.log('errors', errors);
       });
     }
 
