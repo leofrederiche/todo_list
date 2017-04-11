10b804c2f42cf995c79720b087c3bfc00c5ffee5
lleonardoll <leofrederiche@gmail.com>
2017-04-11 15:43:57 -0300

    #0 - Limpando pastas, organizando rotas, removendo testes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root "pages#home"
-  resources :lists
+  root 'pages#home'
+  get 'lists', to: 'lists#index', as: :lists_path
 
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
